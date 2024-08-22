16bfb26388e00dd521aa771bef734d61c1d6f6b7
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-08-22 15:08:19 -0500

    views auth

--- app/auth/views/login.php
+++ app/auth/views/login.php
@@ -1,1 +1,37 @@
-<?='LOGIN'?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesión</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../public/css/auth.css">
+</head>
+<body>
+<div class="container mt-5 ">
+  <div class="d-flex justify-content-center align-items-center">
+      <div class="card p-4">
+        <img src="https://media.licdn.com/dms/image/v2/D560BAQEy1CGTGds_9A/company-logo_200_200/company-logo_200_200/0/1681229472418?e=1732147200&v=beta&t=MU75PKtb8SDV--WZawqINSkuVqWGH50-WawKxVtKjBk" class="card-img-top img-card-login" alt="imagen de fondo inkapaking">
+        <div class="card-body">
+        <h2>Iniciar sesión</h2>
+        <p>¡Bienvenida al sistema de gestión de nuestro comedor!</p>
+      <form action="../../home/views/home.php" method="post">
+          <div class="mb-3">
+              <label for="email" class="form-label">Correo electrónico</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="tu@email.com" required>
+          </div>
+          <div class="mb-3">
+              <label for="password" class="form-label">Contraseña</label>
+              <input type="password" class="form-control" id="password" name="password" required>
+          </div>
+          <div class="d-flex justify-content-between">
+            <button type="submit" class="col-5 btn btn-dark">Iniciar sesión</button>
+            <a href="/app/auth/views/register.php" class="btn btn-light col-5">Regístrate aquí</a>
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
