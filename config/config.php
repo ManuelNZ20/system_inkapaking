c1ea8c782c5d4938354aa78c5c76be50e4a64e13
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-08-23 14:40:51 -0500

    test-fist environment main

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-require_once(__DIR__.'/../vendor/autoload.php');
+// require_once(__DIR__.'/../vendor/autoload.php');
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/');
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/');
+// $dotenv->load();
 
 
 // En Producción
